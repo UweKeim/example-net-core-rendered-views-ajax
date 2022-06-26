fd89d1fd547cdf11689775aeb06e7dd812a699db
Uwe Keim <uwe.keim@zeta-software.de>
2022-06-26 12:30:22 +0200

    feat: Bootstrap Modal with DX.

--- Source/VrseWebApplication/Controllers/TestDevExtremeController.cs
+++ Source/VrseWebApplication/Controllers/TestDevExtremeController.cs
@@ -22,6 +22,12 @@ public sealed class TestDevExtremeController : Controller
         return View();
     }
 
+    [HttpGet]
+    public IActionResult DxTest02()
+    {
+        return View();
+    }
+
     [HttpPost]
     public async Task<IActionResult> GetPartialView01(int id)
     {
@@ -30,6 +36,17 @@ public sealed class TestDevExtremeController : Controller
             {
                 html = await _viewRenderer.RenderPartialView(this, @"Partials/DxPartialView01",
                     new DxTestPartial01ViewModel { Id = id }, ViewData)
+            });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> GetPartialView03(int id)
+    {
+        return Json(
+            new
+            {
+                html = await _viewRenderer.RenderPartialView(this, @"Partials/DxPartialView03",
+                    new DxTestPartial03ViewModel { Id = id }, ViewData)
             });
     }
 
@@ -55,6 +72,34 @@ public sealed class TestDevExtremeController : Controller
             new
             {
                 html = await _viewRenderer.RenderPartialView(this, @"Partials/DxPartialView02", model.Id, ViewData)
+            });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> DxPostPartialView03(DxTestPartial03ViewModel model)
+    {
+        if (model.Id <= 0)
+        {
+            ModelState.AddModelError(string.Empty, "Value must be greater than zero.");
+        }
+
+        if (!ModelState.IsValid)
+        {
+            return Json(
+                new
+                {
+                    error = true,
+                    successful = false,
+                    html = await _viewRenderer.RenderPartialView(this, @"Partials/DxPartialView03", model, ViewData)
+                });
+        }
+
+        return Json(
+            new
+            {
+                error = false,
+                successful = true,
+                message = $"Received ID {model.Id}"
             });
     }
 }
---
+++ Source/VrseWebApplication/Models/DxTestPartial03ViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace VrseWebApplication.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+public sealed class DxTestPartial03ViewModel
+{
+    [Required, Range(1, 1000)]  public int Id { get; set; }
+}
